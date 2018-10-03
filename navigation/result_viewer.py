d17f06943fd0e2ee8cc2ce5bf1871db6f0d89b8b
smikesh <mshtelma@gmail.com>
2018-10-03 22:41:58 +0200

    environments

--- navigation/result_viewer.py
+++ navigation/result_viewer.py
@@ -2,12 +2,12 @@ import pickle
 import itertools
 from unityagents import UnityEnvironment
 
-from navigation import RunningLearningTask, Result, LearningTask, DQN
+from navigation import RunningLearningTask, Result, LearningTask
 
-res = pickle.load(open("res_04_run2.pckl", "rb"))
+res = pickle.load(open("res_05.pckl", "rb"))
 # print(res)
 sorted_res = sorted(res, key=lambda x: x.episode)
 
 for r in sorted_res:
-    if r.mean_score > 13:
-        print(r)
+    #if r.mean_score > 13:
+    print(r.mean_score, r.task)
--- navigation/test5.py
+++ navigation/test5.py
@@ -11,33 +11,32 @@ TAU = 0.0075  # for soft update of target parameters
 LR = 0.001  # learning rate
 UPDATE_EVERY = 4  # how often to update the network
 
-eps_starts = [1.0, 0.5, 0.25, 0.2]
+eps_starts = [ 0.25, 0.2, 1.0, 0.5]
 eps_decays = [0.99, 0.9, 0.8, 0.85]
-eps_ends = [0.001, 0.02, 0.05, 0.08]
+eps_ends = [0.001, 0.02, 0.03]
 lrs = [0.0005, 0.0008, 0.001]
 buffers = [BUFFER_SIZE]
 batches = [64, 128, 256]
 gammas = [GAMMA]
-taus = [ 0.002, 0.007, 0.008, 0.01, 0.015, 0.02]
+taus = [0.002, 0.007, 0.008, 0.01, 0.015, 0.02]
 update_rates = [1, 2, 4]
-agent_types = [ 'DQN', 'DDQN']  # Add
-layers = [ [16, 12, 6], [24, 12, 8], [32, 24], [32, 8], [24,6]]
-network_types = [ 'Simple','Dueling'] #,
-
+agent_types = ['DQN', 'DDQN']  # Add
+layers = [ [32, 24]] #[16, 12, 6], [24, 12, 8], [32, 8], [24, 6]
+network_types = ['Simple', 'Dueling']  # ,
 
 # LearningTask(eps_start=1.0, eps_end=0.01, eps_decay=0.99, lr=0.001, buffer_size=100000, batch_size=64, gamma=0.99, tau=0.01, update_rate=4, agent_type='DQN', layers=[32, 24], network_type='Dueling')
 env = UnityEnvironment(file_name="Environments/Banana.app", no_graphics=False)
 
 tasks = list(itertools.product(
-    *[eps_starts, eps_ends, eps_decays, lrs, buffers, batches, gammas, taus, update_rates, agent_types, layers, network_types]))
+    *[eps_starts, eps_ends, eps_decays, lrs, buffers, batches, gammas, taus, update_rates, agent_types, layers,
+      network_types]))
 res = []
 dqn = RLAlgorithm(env, max_episodes=200)
 for task_spec in tasks:
-
     task = LearningTask._make(task_spec)
     print(task)
     r = dqn.run_dqn(task)
-    print(r)
+    print('episode = ', r.episode, 'mean score = ', r.mean_score)
     res.append(r)
     f = open('res_05.pckl', 'wb')
     pickle.dump(res, f)
